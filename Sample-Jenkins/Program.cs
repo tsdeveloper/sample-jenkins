9873cc1669655b29a79c508ef70d7214247b9c4c
Thyago Developer <ts.analista@gmail.com>
2018-06-26 21:02:56 -0300

    Add project files.

---
+++ Sample-Jenkins/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Jenkins");
+        }
+    }
+}
